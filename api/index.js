767ad98be83e66ab35b179e2a9e846d705e69f50
Moonwalker231102 <ejimenezcadena@gmail.com>
2023-07-29 10:44:01 -0500

    Models and db conection established

--- api/index.js
+++ api/index.js
@@ -18,11 +18,12 @@
 //                       `=---='
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
-const { conn } = require('./src/db.js');
+const { sequelize } = require('./src/db.js');
 
 // Syncing all the models at once.
-conn.sync({ force: true }).then(() => {
+
+
   server.listen(3001, () => {
-    console.log('%s listening at 3001'); // eslint-disable-line no-console
+    sequelize.sync({force: false})
+    console.log('Server listening at 3001'); // eslint-disable-line no-console
   });
-});
--- api/src/models/Dog.js
+++ api/src/models/Dog.js
@@ -1,12 +1,31 @@
-const { DataTypes } = require('sequelize');
-// Exportamos una funcion que define el modelo
-// Luego le injectamos la conexion a sequelize.
+const { DataTypes} = require('sequelize');
+
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('dog', {
+  sequelize.define('Dog', {
+    id:{
+      type: DataTypes.UUID,
+      primaryKey: true,
+      defaultValue: DataTypes.UUIDV4
+    },
+    image:{
+      type: DataTypes.STRING
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
-  });
+    height:{
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    weight:{
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    life_span: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  },{timestamps: false});
 };
